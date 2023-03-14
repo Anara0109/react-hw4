3d858223205d20a381ff5ea49bd2cf7735d7750c
Anara0109 <107864273+Anara0109@users.noreply.github.com>
2023-03-14 16:53:07 +0600

    Add files via upload

---
+++ react-hw4/src/App/components/TodoList/TodoList.tsx
@@ -0,0 +1,27 @@
+import { ITodo } from "../../../types";
+import TodoListItem from "../TodoListItem/TodoListItem";
+import { FC } from "react"
+
+interface ITodoListProps {
+    todos: ITodo[]
+    onDone: (id:number, newState:"done") => void
+    onImportant: (id:number, newState:"important") => void
+    onDelete: (id:number)=> void
+}
+
+const TodoList: FC<ITodoListProps> = (props) => {
+   
+    return (
+        <ul>
+            {props.todos.map( item => (
+                <TodoListItem key={item.id} {...item} 
+                onDoneClick={() => props.onDone(item.id, "done")}
+                onImportantClick={() => props.onImportant(item.id, "important")}
+                onDeleteClick={() => props.onDelete(item.id)}
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default TodoList
